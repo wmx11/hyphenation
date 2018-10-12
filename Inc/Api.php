629160acc9759779d982b95834bf463383fe4338
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-12 14:33:27 +0300

    Changed GET request to GET, POST, PUT, DELETE requests, added filtering

--- Inc/Api.php
+++ Inc/Api.php
@@ -7,10 +7,18 @@ use Inc\Database;
 class Api
 {
     private $db;
+    private $url;
+    private $id;
+    private $api = "api";
+    private $query;
+    private $tableName;
+    private $endPoint;
 
     public function __construct($db)
     {
         $this->db = $db;
+        $this->setUrl();
+        $this->setParameters();
     }
 
     public function __destruct()
@@ -20,67 +28,141 @@ class Api
 
     public function runApi()
     {
-        switch($_SERVER['REQUEST_METHOD']) {
+        switch ($_SERVER['REQUEST_METHOD']) {
             case 'GET':
                 $this->get();
-            break;
+                break;
 
             case 'POST':
                 $this->post();
-            break;
+                break;
 
             case 'PUT':
                 $this->put();
-            break;
+                break;
 
             case 'DELETE':
                 $this->delete();
-            break;
+                break;
 
             default:
                 echo "Access Denied";
-            break;
+                break;
         }
     }
 
-    public function get()
+    private function setUrl()
     {
-        if(isset($_GET['get'])) {
-            $request = $_GET['get'];
-            if ($request === 'patterns') {
-                header("Access-Control-Allow-Methods: GET");
-                header("Content-Type: application/json");
-                $patterns = $this->db->get('id, pattern', 'patterns', "limit 50");
-                print_r(json_encode($patterns));
-            } elseif ($request === 'pattern' && filter_var($_GET['id'], FILTER_VALIDATE_INT) !== false) {
-                $id = $_GET['id'];
-                header("Access-Control-Allow-Methods: GET");
-                header("Content-Type: application/json");
-                $pattern = $this->db->get('id, pattern', 'patterns', "WHERE id = $id");
-                print_r(json_encode($pattern));
+        if (strpos($_SERVER['REQUEST_URI'], "?") === false) {
+            $this->url = explode("/", $_SERVER['REQUEST_URI']);
+        }
+    }
+
+    private function setParameters()
+    {
+        $this->tableName = $this->url[3]; // Sets Table Name
+        if (!empty($this->url[4])): $this->id = $this->url[4]; endif; // Sets ID for Get Request
+        if (!empty($this->url[4]) && is_numeric($this->url[4]) === false): $this->endPoint = $this->url[4]; endif; // Sets Endpoint for Post, Put, Delete Requests
+    }
+
+    private function validateUrlGet()
+    {
+        if (!empty($this->url[2]) && $this->url[2] === $this->api && empty($this->url[4])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function getParameters()
+    {
+        if (strpos($_SERVER['REQUEST_URI'], "?") !== false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function validateAnchor()
+    {
+        if (!empty($this->url[2]) && $this->url[2] === $this->api) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function validateUrlParametersGet()
+    {
+        if (!empty($this->url[4]) && $this->url[2] === $this->api && filter_var($this->url[4], FILTER_VALIDATE_INT)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function buildQuery()
+    {
+        $string = "";
+        foreach ($_GET as $key => $value) {
+            if (is_numeric($value) === true) {
+                $string .= "$key = $value AND ";
+            } else {
+                $string .= "$key = '$value' AND ";
             }
         }
+        $this->query = rtrim($string, " AND") . " ";
+        $this->setTableName();
     }
 
-    public function post()
+    private function setTableName()
     {
-        $insert = $_GET['insert'];
-        $info = json_decode(file_get_contents("php://input"), true);
-        $this->db->insert($insert, $info);
+        $url1 = explode("?", $_SERVER['REQUEST_URI'])[0];
+        $this->tableName = explode("/", $url1)[3];
     }
 
-    public function put()
+    private function get()
     {
-        $table = $_GET['update'];
-        $id = $_GET['id'];
-        $info = json_decode(file_get_contents("php://input"), true);
-        $this->db->update($table, $info, "id = $id");
+        if ($this->validateUrlGet() === true && $this->getParameters() === false) {
+            header("Content-Type: application/json");
+            $patterns = $this->db->get('*', $this->tableName, "limit 50");
+            print_r(json_encode($patterns));
+        } elseif ($this->validateUrlParametersGet() === true && $this->getParameters() === false) {
+            header("Content-Type: application/json");
+            $patterns = $this->db->get('*', $this->tableName, "WHERE id = $this->id");
+            print_r(json_encode($patterns));
+        } elseif ($this->getParameters() === true) {
+            header("Content-Type: application/json");
+            $this->buildQuery();
+            $patterns = $this->db->get('*', $this->tableName, "WHERE $this->query");
+            print_r(json_encode($patterns));
+        }
     }
 
-    public function delete()
+    private function post()
     {
-        $table = $_GET['delete'];
-        $id = $_GET['id'];
-        $this->db->delete($table, "id = $id");
+        if ($this->validateAnchor() === true && $this->endPoint === 'post') {
+            $info = json_decode(file_get_contents("php://input"), true);
+            $this->db->insert($this->tableName, $info);
+        }
+    }
+
+    private function put()
+    {
+        if ($this->validateAnchor() === true && $this->endPoint === 'put') {
+            $info = json_decode(file_get_contents("php://input"), true);
+            $id = $info['id'];
+            $data = array_splice($info, 1, 1);
+            $this->db->update($this->tableName, $data, "id = $id");
+        }
+    }
+
+    private function delete()
+    {
+        if ($this->validateAnchor() === true && $this->endPoint === 'delete') {
+            $info = json_decode(file_get_contents("php://input"), true);
+            $id = $info['id'];
+            $this->db->delete($this->tableName, "id = $id");
+        }
     }
 }
