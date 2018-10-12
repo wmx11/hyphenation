15f11e174b689764b0ce93b6f2e0a20be640d658
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-12 14:32:37 +0300

    Removed Commented Out Code

--- main.php
+++ main.php
@@ -1,15 +1,10 @@
 <?php
 
 use Inc\App;
-//use Inc\Api;
 
 require_once('Inc/Autoload.php');
 
-//$api = new Api();
-//$api->runApi();
-
 $app = new App();
 $app->runApp();
-//$app->displayTime();
 
-//$app->returnDb()->insertTransaction();
+
