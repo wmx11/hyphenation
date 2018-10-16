10aefce798164bfac20645b36d16715625771d88
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-16 16:14:48 +0300

    Removed commented out code

--- main.php
+++ main.php
@@ -1,14 +1,9 @@
 <?php
 
-//use Inc\App;
-
 require_once __DIR__ . '/vendor/autoload.php';
-require_once('Inc/Autoload.php');
+require_once('Inc/Resources/Autoload.php');
 
-$container = new DI\Container();
-//$builder = new DI\ContainerBuilder();
-//$builder->addDefinitions('App');
-$app = $container->get();
+use Inc\App;
 
-//$app = new App();
+$app = new App();
 $app->runApp();
