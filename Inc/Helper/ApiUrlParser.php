e45bf869e6c8f4e58c06dad14b2183a35707d920
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-16 16:09:53 +0300

    Added URL Parser for the API class

--- Inc/Helper/ApiUrlParser.php
+++ Inc/Helper/ApiUrlParser.php
@@ -2,15 +2,86 @@
 
 namespace Inc\Helper;
 
-class ApiUrlParser
-{
-    private $url;
+use Inc\Model\Uri;
 
-    public function setUrl()
+class ApiUrlParser extends Uri
+{
+    private $api = "api";
+
+    public function setTableNameInitial()
     {
-        if (!empty($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], "?") === false) {
-            $this->url = explode("/", $_SERVER['REQUEST_URI']);
-            return $this->url;
+        return $this->segment(3);
+    }
+
+    public function setId()
+    {
+        return $this->segment(4);
+    }
+
+    public function setEndpoint()
+    {
+        if (is_numeric($this->segment(4)) === false) {
+            return $this->segment(4);
         }
+    }
+
+    public function extractTableNameFromGet()
+    {
+        $url = $this->getUrl();
+        if (strpos($url, "?") !== false) {
+            $url_path = explode("?", $url)[0];
+            $tableName = explode('/', $url_path)[3];
+            return $tableName;
+        }
+    }
+
+    public function validateUrlGet()
+    {
+        if ($this->segment(2) === $this->api && empty($this->segment(4)) === true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getParameters()
+    {
+        if (strpos($this->getUrl(), "?") !== false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function validateAnchor()
+    {
+        if ($this->segment(2) === $this->api) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function validateUrlParametersGet()
+    {
+        if ($this->segment(2) === $this->api && filter_var($this->segment(4), FILTER_VALIDATE_INT)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function buildQuery()
+    {
+        $string = "";
+        foreach ($_GET as $column => $filter) {
+            if (is_numeric($filter) === true) {
+                $string .= "$column = $filter AND ";
+            } else {
+                $string .= "$column = '$filter' AND ";
+            }
+        }
+        $query = preg_replace('/[^A-Za-z0-9\-=_ ]/', '', rtrim($string, " AND"));
+        return $query;
     }
 }
