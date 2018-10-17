ef0e176f11f0b98ba3df3cb6bbc7874246aea8e9
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-17 15:39:37 +0300

    Added dependency injection, removed unused variables'

--- Inc/App.php
+++ Inc/App.php
@@ -3,36 +3,34 @@
 namespace Inc;
 
 use Inc\Helper\Logger;
-use Inc\Helper\Timer;
-use Inc\Database\Database;
 use Inc\AppStrategy\AppStrategy;
 
 class App
 {
-    private $api;
-    private $con;
+    private $container;
     private $timer;
     private $logger;
+    private $cache;
 
-    public function __construct()
+    public function __construct($container)
     {
-        $this->timer = new Timer();
+        $this->container = $container;
+        $this->timer = $this->container->inject('Timer');
+        $this->cache = $this->container->inject('Cache');
         $this->logger = Logger::init();
     }
 
     public function runApp()
     {
         if (empty($_SERVER['REQUEST_URI']) !== true) {
-            $this->con = new Database();
-            $this->api = new Api($this->con);
-            $this->api->runApi();
+            $this->container->inject('Api')->runApi();
         } else {
-            echo file_get_contents("Resources/StartMessage.txt") . "\r\n";
+            echo file_get_contents("TextFiles/StartMessage.txt") . "\r\n";
             $input = readline("What would you like to do?: ");
             if (empty($input)) {
                 echo "Type help to display all functions \r\n";
             } else {
-                $app = new AppStrategy($input);
+                $app = new AppStrategy($input, $this->container);
             }
         }
     }
@@ -46,6 +44,6 @@ class App
     public function log()
     {
         $this->logger->logTime($this->timer->getTimeElapsed() . "\r\n");
-        $this->logger->log("Items Cached: " . $this->cache->getNewCachedItems(), "Words Hyphenated: " . $this->words->getWordCount());
+        $this->logger->log("Items Cached: " . $this->cache->getNewCachedItems(), "Words Hyphenated: ");
     }
 }
