e24caf4363fa69bf328b43376bce03558bb09f93
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-17 16:26:46 +0300

    Changed how the Injector works. It now stores path to class in array and initiates it when needed

--- Inc/Injector.php
+++ Inc/Injector.php
@@ -27,11 +27,11 @@ class Injector
     public function setClass($name, $location, $dependency = null, $filepath = null)
     {
         if ($dependency === null && $filepath === null) {
-            $this->container[$name] = [$name => new $location];
+            $this->container[$name] = [$name => $location];
         } elseif ($dependency === null && $filepath !== null) {
-            $this->container[$name] = [$name => new $location($filepath)];
+            $this->container[$name] = [$name => $location . " " . $filepath];
         } else {
-            $this->container[$name] = [$name => new $location(new $dependency)];
+            $this->container[$name] = [$name => $location . " " . $dependency];
         }
     }
 
@@ -42,6 +42,14 @@ class Injector
 
     public function inject($class)
     {
-        return $this->container[$class][$class];
+        $string = $this->container[$class][$class];
+        $object = explode(" ", $string);
+        if (empty($object[0]) !== true && empty($object[1]) === true) {
+            return new $object[0];
+        } elseif (empty($object[1]) !== true && class_exists($object[1]) === true) {
+            return new $object[0](new $object[1]);
+        } elseif (empty($object[1]) !== true && file_exists($object[1]) === true) {
+            return new $object[0]($object[1]);
+        }
     }
 }
