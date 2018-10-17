06d4c86978b997e4e6244edc3cced75b9a01e230
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-17 15:32:11 +0300

    Added Injector class

--- main.php
+++ main.php
@@ -3,7 +3,9 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once('Inc/Resources/Autoload.php');
 
+use Inc\Injector;
 use Inc\App;
 
-$app = new App();
+$container = new Injector();
+$app = new App($container);
 $app->runApp();
