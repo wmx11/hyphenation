ccf6d00e6115cfba8155a966a7799ba6bbde4481
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-18 16:07:46 +0300

    Added Document Root path

--- Inc/Resources/Autoload.php
+++ Inc/Resources/Autoload.php
@@ -4,7 +4,7 @@ spl_autoload_register(function ($className) {
     $filename = str_replace('\\', '/', $className) . '.php';
 
     if (file_exists($filename) === true) {
-        require_once($filename);
+        include($_SERVER['DOCUMENT_ROOT'] . "/$filename");
     } else {
         print "$filename does not exist";
     }
