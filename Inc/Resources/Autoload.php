2c49f876dc1f8ce91a11a567c42f32a3e7c38217
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-19 16:40:06 +0300

    Removed global path

--- Inc/Resources/Autoload.php
+++ Inc/Resources/Autoload.php
@@ -4,7 +4,7 @@ spl_autoload_register(function ($className) {
     $filename = str_replace('\\', '/', $className) . '.php';
 
     if (file_exists($filename) === true) {
-        include($_SERVER['DOCUMENT_ROOT'] . "$filename");
+        include("$filename");
     } else {
         print "$filename does not exist";
     }
