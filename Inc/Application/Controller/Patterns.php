1dfe5bf5df5cd5553156598978f9fe18c3e67a67
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-22 15:39:41 +0300

    Created Pagination object and passed it to views

--- Inc/Application/Controller/Patterns.php
+++ Inc/Application/Controller/Patterns.php
@@ -3,26 +3,30 @@
 namespace Inc\Application\Controller;
 
 use Inc\Application\Core\Controller;
+use Inc\Application\Core\Pagination;
 use Inc\Application\Model\PatternsModel;
 
 class Patterns extends Controller
 {
     private $model;
+    private $pagination;
 
     public function __construct()
     {
         $this->model = new PatternsModel();
+        $this->pagination = new Pagination();
     }
 
     public function home()
     {
-        $patterns = $this->model->getPatterns();
-        $patternCount = $this->model->patternCount()[0]['COUNT(*)'];
+        $data['patterns'] = $this->model->getPatterns();
+        $data['paginate'] = $this->pagination;
+
         echo $this->loadView('head');
         echo $this->loadView('sidebar');
         echo $this->loadView('submitForm');
         echo $this->loadView('editPopup');
-        echo $this->loadView('patterns', $patterns);
+        echo $this->loadView('patterns', $data);
         echo $this->loadView('footer');
     }
 
--- Inc/Application/Controller/Words.php
+++ Inc/Application/Controller/Words.php
@@ -4,24 +4,29 @@ namespace Inc\Application\Controller;
 
 use Inc\Application\Core\Controller;
 use Inc\Application\Model\WordsModel;
+use Inc\Application\Core\Pagination;
 
 class Words extends Controller
 {
     private $model;
+    private $pagination;
 
     public function __construct()
     {
         $this->model = new WordsModel();
+        $this->pagination = new Pagination();
     }
 
     public function home()
     {
-        $words = $this->model->getWords();
+        $data['words'] = $this->model->getWords();
+        $data['paginate'] = $this->pagination;
+
         echo $this->loadView('head');
         echo $this->loadView('sidebar');
         echo $this->loadView('submitForm');
         echo $this->loadView('editPopup');
-        echo $this->loadView('words', $words);
+        echo $this->loadView('words', $data);
         echo $this->loadView('footer');
     }
 
