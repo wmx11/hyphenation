de0d2c7db369b2b9f2418a68af9a13f65a424bf2
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-22 13:56:56 +0300

    Added edit popup window

--- Inc/Application/View/patterns.php
+++ Inc/Application/View/patterns.php
@@ -3,11 +3,11 @@
         <div class="header">
             <h1>Patterns</h1>
         </div>
-        <?php foreach($data as $key => $row):?>
-        <div class="showResult">
-            <div class="result" id="pattern<?php echo $key;?>"> <?php echo $row['pattern']?> </div>
-            <div class="edit"><a href="#">Edit</a></div>
-            <div class="delete"><a href="#" id="delete" onclick="remove()">Delete</a></div>
+        <?php foreach($data as $id => $row):?>
+        <div class="showResultPattern" id="<?php echo $id;?>">
+            <div class="result" id="<?php echo $id;?>"> <?php echo $row['pattern']?> </div>
+            <div class="edit" id="<?php echo $id;?>"><a href="#">Edit</a></div>
+            <div class="delete" id="<?php echo $id;?>"><a href="#">Delete</a></div>
         </div>
         <?php endforeach;?>
     </div>
--- Inc/Application/View/words.php
+++ Inc/Application/View/words.php
@@ -3,11 +3,11 @@
         <div class="header">
             <h1>Words</h1>
         </div>
-        <?php foreach($data as $row):?>
-        <div class="showResult">
-            <div class="result"><?php echo $row['word'];?></div>
-            <div class="edit"><a href="#">Edit</a></div>
-            <div class="delete"><a href="#">Delete</a></div>
+        <?php foreach($data as $id => $row):?>
+        <div class="showResultWord" id="<?php echo $id;?>">
+            <div class="result" id="<?php echo $id;?>"><?php echo $row['word'];?></div>
+            <div class="edit" id="<?php echo $id;?>"><a href="#">Edit</a></div>
+            <div class="delete" id="<?php echo $id;?>"><a href="#">Delete</a></div>
         </div>
         <?php endforeach;?>
     </div>
