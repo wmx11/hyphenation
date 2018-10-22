f988416fca9e654e760f656d466e2fe10ee01ac8
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-22 13:54:37 +0300

    Moved script.js from header to footer

--- Inc/Application/View/footer.php
+++ Inc/Application/View/footer.php
@@ -1,38 +1,3 @@
-<script>
-    $(document).ready(function(){
-        $('#form').on('submit', function(e){
-            var word = $('#word').val();
-            var select = $('#select').val();
-            var string = 'table='+select+'&word='+word;
-            if (word == '' || select =='') {
-                e.preventDefault();
-                alert("Field is required");
-            } else {
-                $.ajax({
-                    type: "POST",
-                    url: '/main.php/Words/submit',
-                    data: string,
-                    success: function(){
-                        console.log('Submitted');
-                    }
-                });
-            }
-        });
-    });
-
-    function display(){
-        var button = document.getElementById("addNew");
-        var form = document.getElementById("submitForm");
-
-        form.style.display = "block";
-    }
-
-    function remove(){
-        //var remove = document.getElementById("remove");
-        var result = document.getElementsByClassName('result');
-        alert(result.length);
-    }
-
-</script>
+<script src="../../../Assets/script.js"></script>
 </body>
 </html>
