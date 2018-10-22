0d0c7ddaed89c05ea4cced44971167801371542a
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-22 15:36:30 +0300

    Fixed indentations

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,10 @@
 //Add new word or pattern
-$(document).ready(function(){
-    $('#form').on('submit', function(e){
+$(document).ready(function () {
+    $('#form').on('submit', function (e) {
         var word = $('#word').val();
         var select = $('#select').val();
-        var string = 'table='+select+'&word='+word;
-        if (word == '' || select =='') {
+        var string = 'table=' + select + '&word=' + word;
+        if (word == '' || select == '') {
             e.preventDefault();
             alert("Field is required");
         } else {
@@ -12,7 +12,7 @@ $(document).ready(function(){
                 type: "POST",
                 url: '/main.php/Words/submit',
                 data: string,
-                success: function(){
+                success: function () {
                     console.log('Submitted');
                 }
             });
@@ -21,7 +21,7 @@ $(document).ready(function(){
 });
 
 //Display add form
-function display(){
+function display() {
     var button = document.getElementById("addNew");
     var form = document.getElementById("submitForm");
 
@@ -29,18 +29,18 @@ function display(){
 }
 
 //Pattern Delete
-$(document).ready(function() {
-    $(".showResultPattern .delete").click(function() {
+$(document).ready(function () {
+    $(".showResultPattern .delete").click(function () {
         var patternId = this.id;
-        var result = $("#"+patternId+".result").text();
-        var resultWrapper = $("#"+patternId+".showResultPattern");
-        var string = 'pattern='+result;
+        var result = $("#" + patternId + ".result").text();
+        var resultWrapper = $("#" + patternId + ".showResultPattern");
+        var string = 'pattern=' + result;
         $.ajax({
             type: "POST",
             url: "/main.php/patterns/delete",
             data: string,
-            success: function(){
-                resultWrapper.fadeOut(400, function() {
+            success: function () {
+                resultWrapper.fadeOut(400, function () {
                     resultWrapper.remove()
                 });
             }
@@ -49,18 +49,18 @@ $(document).ready(function() {
 });
 
 //Word Delete
-$(document).ready(function() {
-    $(".showResultWord .delete").click(function() {
+$(document).ready(function () {
+    $(".showResultWord .delete").click(function () {
         var wordId = this.id;
-        var result = $("#"+wordId+".result").text();
-        var resultWrapper = $("#"+wordId+".showResultWord");
-        var string = 'word='+result;
+        var result = $("#" + wordId + ".result").text();
+        var resultWrapper = $("#" + wordId + ".showResultWord");
+        var string = 'word=' + result;
         $.ajax({
             type: "POST",
             url: "/main.php/words/delete",
             data: string,
-            success: function(){
-                resultWrapper.fadeOut(400, function() {
+            success: function () {
+                resultWrapper.fadeOut(400, function () {
                     resultWrapper.remove()
                 });
             }
@@ -69,52 +69,52 @@ $(document).ready(function() {
 });
 
 // Word Editing
-$(document).ready(function() {
-    $(".showResultWord .edit").mouseup(function() {
+$(document).ready(function () {
+    $(".showResultWord .edit").mouseup(function () {
         $("#editPopup").show().css('display', 'flex');
         var wordId = this.id;
-        var result = $("#"+wordId+".result").text();
-        var resultWrapper = $("#"+wordId+".showResultWord");
+        var result = $("#" + wordId + ".result").text();
+        var resultWrapper = $("#" + wordId + ".showResultWord");
         $("#editWord").val(result);
 
-        $("#editForm").on('submit', function() {
+        $("#editForm").on('submit', function () {
             $.ajax({
                 type: "POST",
                 url: "/main.php/words/edit",
-                data: 'word='+result+'&editedWord='+$("#editWord").val(),
-                success: function() {
+                data: 'word=' + result + '&editedWord=' + $("#editWord").val(),
+                success: function () {
                 }
             });
         });
     });
-    $("#editPopup").click(function(e) {
-        if(!$(e.target).closest("#editWord").length) {
+    $("#editPopup").click(function (e) {
+        if (!$(e.target).closest("#editWord").length) {
             $("#editPopup").fadeOut(100);
         }
     })
 });
 
 //Pattern Editing
-$(document).ready(function() {
-    $(".showResultPattern .edit").mouseup(function() {
+$(document).ready(function () {
+    $(".showResultPattern .edit").mouseup(function () {
         $("#editPopup").show().css('display', 'flex');
         var wordId = this.id;
-        var result = $("#"+wordId+".result").text();
-        var resultWrapper = $("#"+wordId+".showResultPattern");
+        var result = $("#" + wordId + ".result").text();
+        var resultWrapper = $("#" + wordId + ".showResultPattern");
         $("#editWord").val(result);
 
-        $("#editForm").on('submit', function() {
+        $("#editForm").on('submit', function () {
             $.ajax({
                 type: "POST",
                 url: "/main.php/patterns/edit",
-                data: 'pattern='+result+'&editedPattern='+$("#editWord").val(),
-                success: function() {
+                data: 'pattern=' + result + '&editedPattern=' + $("#editWord").val(),
+                success: function () {
                 }
             });
         });
     });
-    $("#editPopup").click(function(e) {
-        if(!$(e.target).closest("#editWord").length) {
+    $("#editPopup").click(function (e) {
+        if (!$(e.target).closest("#editWord").length) {
             $("#editPopup").fadeOut(100);
         }
     })
