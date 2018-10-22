ed6975b64e166303c54a117a6858952a2f2e53cd
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-22 13:54:02 +0300

    Added Edit Popup to the application

---
+++ Inc/Application/View/editPopup.php
@@ -0,0 +1,8 @@
+<div id="editPopup">
+    <div class="updateWrapper">
+        <form action="" method="post" id="editForm">
+            <input type="text" id="editWord">
+            <input type="submit" value="Update">
+        </form>
+    </div>
+</div>
