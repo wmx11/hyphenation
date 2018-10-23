931298ac83a552efb99d7393e839f672659cc972
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-23 11:43:33 +0300

    Removed unused Methods class

--- Inc/Application/Controller/Index.php
+++ Inc/Application/Controller/Index.php
@@ -3,16 +3,15 @@
 namespace Inc\Application\Controller;
 
 use Inc\Application\Core\Controller;
-use Inc\Application\Core\Model;
 
 class Index extends Controller
 {
     public function home()
     {
-        echo $this->loadView('head');
-        echo $this->loadView('sidebar');
-        echo $this->loadView('submitForm');
-        echo $this->loadView('index');
-        echo $this->loadView('footer');
+        $this->loadView('head');
+        $this->loadView('sidebar');
+        $this->loadView('submitForm');
+        $this->loadView('index');
+        $this->loadView('footer');
     }
 }
