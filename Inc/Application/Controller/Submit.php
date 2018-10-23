0e23803f75ee25efed6e048bf406998d7efceec7
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-23 14:11:24 +0300

    Added a new controller to handle submit/insert requests

---
+++ Inc/Application/Controller/Submit.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Inc\Application\Controller;
+
+use Inc\Application\Core\Controller;
+use Inc\Application\Model\TableModel;
+
+class Submit extends Controller
+{
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new TableModel();
+    }
+
+    public function insert()
+    {
+        $tableName = $_POST['table'];
+        $word = $_POST['word'];
+        $this->model->insertWord($tableName, $word);
+    }
+
+    public function hyphenate()
+    {
+
+    }
+}
