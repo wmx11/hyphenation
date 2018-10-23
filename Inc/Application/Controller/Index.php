caba9ad684cdd0cf0579441fbad87ca669d1ee4b
Vainius Mykolaitis <vmykolaitis1@gmail.com>
2018-10-23 14:14:53 +0300

    Added Pattern Count and Word Count to the index page

--- Inc/Application/Controller/Index.php
+++ Inc/Application/Controller/Index.php
@@ -3,15 +3,26 @@
 namespace Inc\Application\Controller;
 
 use Inc\Application\Core\Controller;
+use Inc\Application\Model\IndexModel;
 
 class Index extends Controller
 {
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new IndexModel();
+    }
+
     public function home()
     {
+        $data['patternCount'] = $this->model->getPattersCount();
+        $data['wordCount'] = $this->model->getWordsCount();
+
         $this->loadView('head');
         $this->loadView('sidebar');
         $this->loadView('submitForm');
-        $this->loadView('index');
+        $this->loadView('index', $data);
         $this->loadView('footer');
     }
 }
